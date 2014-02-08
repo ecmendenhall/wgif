3b3bea2556b9fe62d7ec724baea17723f106e1cb
Connor Mendenhall <ecmendenhall@gmail.com>
2014-02-07 23:23:09 -0600

    Use WGif::DownloadBar.

--- lib/wgif/downloader.rb
+++ lib/wgif/downloader.rb
@@ -1,68 +1,69 @@
 require 'viddl-rb'
 require 'typhoeus'
-require 'ruby-progressbar'
+require 'wgif/download_bar'
 require 'wgif/video'
+require 'wgif/video_cache'
 require 'uri'
 require 'cgi'
 
 module WGif
   class Downloader
 
-    def self.video_url youtube_url
-      ViddlRb.get_urls(youtube_url).first
+    def initialize
+      @cache = WGif::VideoCache.new
     end
 
-    def self.video_id youtube_url
+    def video_url youtube_url
+      begin
+        urls = ViddlRb.get_urls(youtube_url)
+        urls.first
+      rescue
+        raise WGif::VideoNotFoundException
+      end
+    end
+
+    def video_id youtube_url
       uri = URI(youtube_url)
       params = CGI.parse(uri.query)
       params['v'].first
     end
 
-    def self.get_video youtube_url
+    def get_video youtube_url
       id = video_id youtube_url
-      if cached? id
-        return cached_clip id
+      if cached_clip = @cache.get(id)
+        return cached_clip
       else
         temp = load_clip(id, youtube_url)
-        WGif::Video.new(id, temp.path)
+        video = WGif::Video.new(id, temp.path)
+        video
       end
     end
 
     private
 
-    def self.cached? id
-      File.exist? "/tmp/wgif/#{id}"
-    end
-
-    def self.cached_clip id
-      WGif::Video.new(id, "/tmp/wgif/#{id}")
-    end
-
-    def self.create_progress_bar request
+    def create_progress_bar request, output_file
       size = nil
-      progress_bar = ProgressBar.create(format: '%e %B %p%% %t',
-                                        smoothing: 0.8,
-                                        total: size)
+      download_bar = WGif::DownloadBar.new
 
       request.on_headers do |response|
         size = response.headers['Content-Length'].to_i
-        progress_bar.total = size
+        download_bar.update_total(size)
       end
 
       request.on_body do |chunk|
           output_file.write(chunk)
-          progress_bar.progress += chunk.size
+          download_bar.increment_progress(chunk.size)
       end
     end
 
-    def self.request_clip youtube_url, output_file
+    def request_clip youtube_url, output_file
       clip_url = self.video_url youtube_url
       request = Typhoeus::Request.new clip_url
-      create_progress_bar(request)
+      create_progress_bar(request, output_file)
       request.run
     end
 
-    def self.load_clip id, youtube_url
+    def load_clip id, youtube_url
       FileUtils.mkdir_p "/tmp/wgif"
       temp = File.open("/tmp/wgif/#{id}", 'wb')
       begin
--- spec/wgif/downloader_spec.rb
+++ spec/wgif/downloader_spec.rb
@@ -3,29 +3,31 @@ require 'wgif/downloader'
 require 'wgif/video'
 
 describe WGif::Downloader do
-
+  let(:downloader) { described_class.new }
   let(:clip_url) { 'http://lol.wut/watch?v=roflcopter' }
 
   before do
     FileUtils.rm_rf('/tmp/wgif')
+    download_bar = double(WGif::DownloadBar).as_null_object
+    WGif::DownloadBar.stub(:new).and_return(download_bar)
   end
 
   it 'retrieves a YouTube download URL' do
     ViddlRb.should_receive(:get_urls).with(clip_url).and_return(['clip url'])
-    described_class.video_url(clip_url).should eq('clip url')
+    downloader.video_url(clip_url).should eq('clip url')
   end
 
   it 'retrieves a YouTube video ID' do
-    described_class.video_id(clip_url).should eq('roflcopter')
+    downloader.video_id(clip_url).should eq('roflcopter')
   end
 
   it 'throws an error if the video is not found' do
     ViddlRb.should_receive(:get_urls).with(clip_url).and_return([])
-    expect{ described_class.get_video(clip_url) }.to raise_error(WGif::VideoNotFoundException)
+    expect{ downloader.get_video(clip_url) }.to raise_error(WGif::VideoNotFoundException)
   end
 
   it 'extracts a YouTube ID from a URL' do
-    described_class.video_id("https://www.youtube.com/watch?v=tmNXKqeUtJM").should eq("tmNXKqeUtJM")
+    downloader.video_id("https://www.youtube.com/watch?v=tmNXKqeUtJM").should eq("tmNXKqeUtJM")
   end
 
   context 'downloading videos' do
@@ -42,13 +44,15 @@ describe WGif::Downloader do
     end
 
     it 'downloads a clip' do
-      WGif::Video.should_receive(:new).with('roflcopter', "/tmp/wgif/roflcopter")
-      video = described_class.get_video(clip_url)
+      video = double(name: 'video')
+      WGif::Video.should_receive(:new).with('roflcopter', "/tmp/wgif/roflcopter").
+        and_return(video)
+      video = downloader.get_video(clip_url)
     end
 
     it 'does not download the clip when already cached' do
-      described_class.get_video(clip_url)
-      described_class.get_video(clip_url)
+      downloader.get_video(clip_url)
+      downloader.get_video(clip_url)
     end
   end
 
