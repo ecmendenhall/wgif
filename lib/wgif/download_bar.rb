13f101ecb96dabe63f532ec1440387c014648601
Connor Mendenhall <ecmendenhall@gmail.com>
2014-02-07 23:19:20 -0600

    Add download bar.

---
+++ lib/wgif/download_bar.rb
@@ -0,0 +1,28 @@
+require 'ruby-progressbar'
+
+module WGif
+  class DownloadBar
+
+    FORMAT = '==> %p%% |%B|'
+    SMOOTHING = 0.8
+
+    attr_reader :progress_bar
+
+    def initialize
+      @progress_bar = ProgressBar.create(
+        format: FORMAT,
+        smoothing: SMOOTHING,
+        total: @size
+      )
+    end
+
+    def update_total(size)
+      @progress_bar.total = size
+    end
+
+    def increment_progress(size)
+      @progress_bar.progress += size
+    end
+
+  end
+end
