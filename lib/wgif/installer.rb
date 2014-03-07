087770c0d7b302de54a54cf2c2ab2cdaf45ed920
Connor Mendenhall <ecmendenhall@gmail.com>
2014-03-07 17:11:07 -0600

    Polish installer.

--- lib/wgif/installer.rb
+++ lib/wgif/installer.rb
@@ -1,17 +1,27 @@
 module WGif
   class Installer
 
-    DEPENDENCIES = [['ffmpeg', 'ffmpeg'], ['imagemagick', 'convert']]
+    DEPENDENCIES = [['ffmpeg', 'ffmpeg'],
+                    ['imagemagick', 'convert']]
 
     def run
+      if dependencies_installed?
+        puts "All dependencies are installed. Go make a GIF."
+        Kernel.exit 0
+      end
       if homebrew_installed?
         DEPENDENCIES.each do |dependency, binary|
           install(dependency, binary)
         end
       else
         puts "WGif can't find Homebrew. Visit http://brew.sh/ to get it."
-        Kernel.exit 0
+        Kernel.exit 1
       end
+      Kernel.exit 0
+    end
+
+    def dependencies_installed?
+      DEPENDENCIES.map {|_, binary| installed?(binary)}.inject(:&)
     end
 
     def homebrew_installed?
@@ -21,7 +31,7 @@ module WGif
     def install(dependency, binary)
       unless installed?(binary)
         puts "Installing #{dependency}..."
-        Kernel.system "brew install #{dependency} > /dev/null"
+        Kernel.system "brew install #{dependency}"
         puts "Successfully installed #{dependency}."
       end
     end
--- spec/wgif/installer_spec.rb
+++ spec/wgif/installer_spec.rb
@@ -29,19 +29,39 @@ describe WGif::Installer do
 
   end
 
+  context 'checking for dependencies' do
+
+    it 'returns true if dependencies are installed' do
+      expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').
+        and_return(true)
+      expect(Kernel).to receive(:system).with('which convert > /dev/null').
+        and_return(true)
+      expect(installer.dependencies_installed?).to eq(true)
+    end
+
+    it 'returns false if dependencies are not installed' do
+      expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').
+        and_return(true)
+      expect(Kernel).to receive(:system).with('which convert > /dev/null').
+        and_return(false)
+      expect(installer.dependencies_installed?).to eq(false)
+    end
+
+  end
+
   context 'installing dependencies' do
 
     it 'does not install dependencies if they are found' do
       expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').
         and_return(true)
-      expect(Kernel).not_to receive(:system).with('brew install ffmpeg > /dev/null')
+      expect(Kernel).not_to receive(:system).with('brew install ffmpeg')
       installer.install('ffmpeg', 'ffmpeg')
     end
 
     it 'installs dependencies' do
       expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').
         and_return(false)
-      expect(Kernel).to receive(:system).with('brew install ffmpeg > /dev/null')
+      expect(Kernel).to receive(:system).with('brew install ffmpeg')
       installer.install('ffmpeg', 'ffmpeg')
     end
 
@@ -63,15 +83,19 @@ describe WGif::Installer do
     it 'installs all dependencies' do
       expect(Kernel).to receive(:system).with('brew info > /dev/null').
         and_return(true)
+      expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').twice.
+        and_return(true)
+      expect(Kernel).to receive(:system).with('which convert > /dev/null').twice.
+        and_return(false)
+      expect(Kernel).to receive(:system).with('brew install imagemagick')
+      expect{ installer.run }.to raise_error(SystemExit)
+    end
+
+    it 'prints a helpful error if homebrew is not found' do
       expect(Kernel).to receive(:system).with('which ffmpeg > /dev/null').
         and_return(true)
       expect(Kernel).to receive(:system).with('which convert > /dev/null').
         and_return(false)
-      expect(Kernel).to receive(:system).with('brew install imagemagick > /dev/null')
-      installer.run
-    end
-
-    it 'prints a helpful error if homebrew is not found' do
       expect(Kernel).to receive(:system).with('brew info > /dev/null').
         and_return(false)
       expect{ installer.run }.to raise_error(SystemExit)
